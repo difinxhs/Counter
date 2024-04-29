bf15aa7794e10b83898c1b476d4d7bebe170e506
difinxhs <8.kchayyy@gmail.com>
2024-04-30 00:10:04 +0200

    Поправил код в соответствии с правками

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -1,42 +1,37 @@
-//
-//  ViewController.swift
-//  Counter
-//
-//  Created by Alex Sacopini on 24.4.24..
-//
-
 import UIKit
 import Foundation
 
 class ViewController: UIViewController {
 
-  
-   
-  
-    @IBOutlet weak var buttonAddOne: UIButton!
+    @IBOutlet weak private var buttonAddOne: UIButton!
     
-    @IBOutlet weak var buttonRemoveOne: UIButton!
+    @IBOutlet weak private var buttonRemoveOne: UIButton!
     
-    @IBOutlet weak var counterLable: UILabel!
+    @IBOutlet weak private var counterLable: UILabel!
     
-    @IBOutlet weak var buttonClear: UIButton!
+    @IBOutlet weak private var buttonClear: UIButton!
     
-    @IBOutlet weak var changeHistory: UITextView!
+    @IBOutlet weak private var changeHistory: UITextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
         buttonAddOne.setImage(UIImage(systemName: "plus"), for: .normal)
+        
         buttonAddOne.setTitle("", for: .normal)
+        
         buttonAddOne.backgroundColor = .red
         
         buttonRemoveOne.setImage(UIImage(systemName: "minus"), for: .normal)
+        
         buttonRemoveOne.setTitle("", for: .normal)
+        
         buttonRemoveOne.backgroundColor = .blue
         
         counterLable.text = "Значение счетчика: 0"
         
         buttonClear.setImage(UIImage(systemName: "trash"), for: .normal)
+        
         buttonClear.setTitle("", for: .normal)
         
         changeHistory.text = "История изменений:"
@@ -44,30 +39,24 @@ class ViewController: UIViewController {
     }
     
     private var counter: Int = 0
+    private var dateFormatter = DateFormatter()
+    private var date = Date()
+    private var changeHistoryArray: [String] = ["История изменений: \n"]
     
-  
-    
-    var dateFormatter = DateFormatter()
-    var date = Date()
-    
-    var changeHistoryArray: [String] = ["История изменений: \n"]
-    
-    func currentDate () -> String {
+    private func currentDate () -> String {
         
         let dateToday = Date()
         
         dateFormatter.dateStyle = .medium
         dateFormatter.timeStyle = .medium
-        
-        dateFormatter.locale = Locale(identifier: "ru_RU")
-        
+    
         let dateToday1 = dateFormatter.string(from: dateToday)
         
         return dateToday1
     }
     
     
-    @IBAction func buttonAddOneDidTap(_ sender: Any) {
+    @IBAction private func buttonAddOneDidTap(_ sender: Any) {
         counter += 1
         counterLable.text = "Значение счетчика: \(counter)"
       
@@ -79,7 +68,7 @@ class ViewController: UIViewController {
         changeHistory.scrollRangeToVisible(range)
     }
     
-    @IBAction func buttonRemoveOneDidTap(_ sender: Any) {
+    @IBAction private func buttonRemoveOneDidTap(_ sender: Any) {
         if counter > 0 {
             counter -= 1
             counterLable.text = "Значение счетчика: \(counter)"
@@ -103,7 +92,7 @@ class ViewController: UIViewController {
         }
     }
     
-    @IBAction func buttonClearDidTap(_ sender: Any) {
+    @IBAction private func buttonClearDidTap(_ sender: Any) {
         counter = 0
         counterLable.text = "Значение счетчика: \(counter)"
       
@@ -114,9 +103,5 @@ class ViewController: UIViewController {
         let range = NSMakeRange(changeHistory.text.count - 1, 0)
         changeHistory.scrollRangeToVisible(range)
     }
-    
-    
-
-
 }
 
